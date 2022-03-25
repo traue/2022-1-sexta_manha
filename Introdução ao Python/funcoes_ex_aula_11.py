0ac0b5aa744ce114ff90b586b949053964f7ca10
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-03-25 09:34:53 -0300

    Continuação

---
+++ Introdução ao Python/funcoes_ex_aula_11.py
@@ -0,0 +1,10 @@
+def leitura(mensagem):
+    frase = input(mensagem)
+    return(f'Você digitou: {frase}')
+
+def leitura_float(mensagem):
+    valor = float(input(mensagem))
+    return (f'Você digitou: {valor}')
+
+
+
